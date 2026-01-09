f669bd8ff63b1c727e62b4d879d47e82e284213b
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-01-09 13:49:17 -0300

    fix: change database and requiments.txt again

--- app/database.py
+++ app/database.py
@@ -6,20 +6,17 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-# Pega a URL do ambiente (Vercel ou .env)
+# Pega a URL do ambiente
 uri = os.getenv("DATABASE_URL")
 
 if not uri:
     raise ValueError("A variável DATABASE_URL não foi encontrada.")
 
-# Ajuste crucial: PostgreSQL exige que a URL comece com postgresql://
-# E forçamos o uso do driver psycopg2 que você instalou
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql+psycopg2://", 1)
 elif uri.startswith("postgresql://"):
     uri = uri.replace("postgresql://", "postgresql+psycopg2://", 1)
 
-# Removemos o "check_same_thread" pois ele quebra no Postgres
 engine = create_engine(uri)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
