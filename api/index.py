dc542ac7f5dfdaa32e57f4c7c0847ea7f390414a
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-01-10 19:18:45 -0300

    fix: fix error cors from back

--- api/index.py
+++ api/index.py
@@ -1,7 +1,20 @@
-from app import database, models, routers
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app import database, models, routers
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="SmartMart API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(routers.router)
