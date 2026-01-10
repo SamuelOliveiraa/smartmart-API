a8a6c1cc3bf14bcb2f4f246b75b8d44edd1534d8
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-01-10 19:01:26 -0300

    fix: error cors on front

--- app/main.py
+++ app/main.py
@@ -7,19 +7,12 @@ models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="SmartMart API")
 
-# Defina as origens permitidas
-origins = [
-    "http://localhost:5173",  # Remover em produção
-    "https://smart-mart-solutions.vercel.app/",
-    "https://smart-mart-solutions.vercel.app",
-]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
 
 app.include_router(routers.router)
